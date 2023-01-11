78bc432c5d000476a64492767b86b854a2a38b8a
HishamReffat <hisham.reffat@e-motionegypt.com>
2023-01-11 16:46:53 +0200

    shoelist and menu

--- app/src/main/java/com/hishamreffat/shoestore/models/ShoesListViewModel.kt
+++ app/src/main/java/com/hishamreffat/shoestore/models/ShoesListViewModel.kt
@@ -9,11 +9,31 @@ class ShoesListViewModel : ViewModel() {
     data class ShoeItem(val name: String)
 
     private var _shoeList = MutableLiveData<MutableList<ShoeItem>>()
+  //  private lateinit var _itemList: MutableList<ShoeItem>
+
     val shoeList: LiveData<MutableList<ShoeItem>>
         get() = _shoeList
 
     init {
-        _shoeList.value = mutableListOf(ShoeItem(name = "first"))
+        _shoeList.value = mutableListOf()
+        for (i in 1..30) {
+           // _itemList.add(ShoeItem("Shoe Number $i"))
+          //  Log.i("cccccccc", "List:$_itemList ")
+          //  _shoeList.value?.add()
+
+//                ShoeItem(name = "first"),
+//                ShoeItem(name = "second"),
+//                ShoeItem(name = "third"),
+//                ShoeItem(name = "fourth"),
+//                ShoeItem(name = "fifth"),
+//                ShoeItem(name = "six"),
+//                ShoeItem(name = "seven"),
+//                ShoeItem(name = "eight"),
+//                ShoeItem(name = "nein"),
+//                ShoeItem(name = "ten"),
+            _shoeList.value!!.add(ShoeItem("Shoe Number $i"))
+        }
+
         Log.i("ShowListViewModel", "ShoesViewModel Created")
     }
 }
